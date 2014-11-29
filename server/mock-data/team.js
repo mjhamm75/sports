74d359e0622a4ef4b0e1bdfc8fb2594d0d25c861
kurtcorbett <kurtcorbett@gmail.com>
2014-11-29 06:17:22 +0000

    Add new mock data for teams

---
+++ server/mock-data/team.js
@@ -0,0 +1,18 @@
+var data = {
+	team: {
+		name: "McGyvers",
+		sport: "Basketball"
+	},
+	teams: [{
+        name: "Spartans",
+        sport: "Football"
+	},{
+        name: "Cowboys",
+        sport: "Football"
+	},{
+        name: "Nationals",
+        sport: "Football"
+	}]
+};
+
+module.exports = data;
