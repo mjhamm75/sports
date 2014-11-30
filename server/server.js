8667536b9438da64c16450f8d807c2728be6076b
kurtcorbett <kurtcorbett@gmail.com>
2014-11-30 02:53:31 +0000

    Reorganize routes

--- server/server.js
+++ server/server.js
@@ -5,12 +5,11 @@ var team = require('./routes/team.js');
 var app = express();
 var PORT = process.env.PORT || 5050;
 
+app.use(express.static(__dirname + '/build'));
+
 app.get('/', function(req,res) {
   res.sendFile('/index.html', {root: __dirname});
 });
-
-app.use(express.static(__dirname + '/build'));
-
 app.get('/users/:userId', user.getUser);
 app.get('/users', user.getUsers);
 app.post('/users', user.createUser);
