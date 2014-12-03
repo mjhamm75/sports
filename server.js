45c3b2e28c4ceabf1e9ba3c0443db6400f4f4d07
kurtcorbett <kurtcorbett@gmail.com>
2014-12-03 06:21:06 +0000

    Moved index.html and server.js to root and changed all references to point to proper path

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var express = require('express');
-var user = require('./routes/user.js');
-var team = require('./routes/team.js');
+var user = require('./server/routes/user.js');
+var team = require('./server/routes/team.js');
 
 var app = express();
 var PORT = process.env.PORT || 5050;
 
-app.use(express.static(__dirname + '/build'));
+app.use(express.static(__dirname));
 
 app.get('/', function(req,res) {
   res.sendFile('/index.html', {root: __dirname});
