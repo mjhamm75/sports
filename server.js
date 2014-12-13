e39f0bcf4fac2cc94c0aa4f13208cbeb316f8c3e
kurtcorbett <kurtcorbett@gmail.com>
2014-12-13 22:06:25 +0000

    corrected require route

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express = require('express'),
     user = require('./server/routes/user.js'),
-    team = require('./server/routes/team.js'),
     pg = require('pg');
 
 var app = express();
@@ -16,7 +15,6 @@ app.get('/', function(req,res) {
 app.get('/users/:id', user.getUser);
 app.get('/users', user.getUsers);
 app.post('/users', user.createUser);
-app.get('/teams', team.getTeams);
 
 
 app.listen(PORT, process.env.IP,function() {
