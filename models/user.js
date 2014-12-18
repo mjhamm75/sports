0ce7e674dfeb1cfeeda6fa1ed310368b7768c793
kurtcorbett <kurtcorbett@gmail.com>
2014-12-18 08:19:33 +0000

    moved files

---
+++ models/user.js
@@ -0,0 +1,35 @@
+var db = require("../index.js");
+
+
+var User =  db.sequelize.define("User", {
+      id: {
+        type: db.Sequelize.INTEGER,
+        primaryKey: true
+      },
+      first_name: {
+        type: db.Sequelize.STRING
+      },
+      last_name: {
+        type: db.Sequelize.STRING
+      },
+      gender: {
+        type: db.Sequelize.STRING
+      },
+      dob: {
+        type: db.Sequelize.DATE
+      },
+      bio: {
+        type: db.Sequelize.STRING
+      },
+      pic: {
+        type: db.Sequelize.BLOB
+      },
+      contact_info_id: {
+        type: db.Sequelize.INTEGER
+      }
+    }, {
+      tableName: "users",
+      timestamps: false
+    });
+
+module.exports = User;
